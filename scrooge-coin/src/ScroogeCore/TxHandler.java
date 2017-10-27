ade3d7845e348f5f8f57ad38b948a262191df5c4
Ramneek Handa <RamneekHanda@gmail.com>
2017-10-28 00:15:37 +0800

    some changes - still not done

--- scrooge-coin/src/ScroogeCore/TxHandler.java
+++ scrooge-coin/src/ScroogeCore/TxHandler.java
@@ -1,10 +1,16 @@
 package ScroogeCore;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.*;
 
 public class TxHandler {
     UTXOPool pool;
+
+    public enum ThreeState {
+        TRUE,
+        FALSE,
+        MAYBE
+    };
+
     /**
      * Creates a public ledger whose current ScroogeCore.UTXOPool (collection of unspent transaction outputs) is
      * {@code utxoPool}. This should make a copy of utxoPool by using the ScroogeCore.UTXOPool(ScroogeCore.UTXOPool uPool)
@@ -61,17 +67,174 @@ public class TxHandler {
         return true;
     }
 
+    public ThreeState isValidTxV2(Transaction tx) {
+        HashSet<UTXO> utxoSet = new HashSet<>();
+        double sumOfInputVals = 0, sumOfOutputVals = 0;
+        int i = 0;
+        for (Transaction.Input input: tx.getInputs()) {
+            UTXO lastUTXO = new UTXO(input.prevTxHash, input.outputIndex);
+            Transaction.Output prevTx = pool.getTxOutput(lastUTXO);
+            // check 1 - all output claimed by tx are in current utxopool
+            if (!pool.contains(lastUTXO)) {
+                return ThreeState.MAYBE;
+            }
+            // check 2 - signatures of each input are valid
+            if (input.signature == null || !Crypto.verifySignature(prevTx.address, tx.getRawDataToSign(i), input.signature)) {
+                return ThreeState.FALSE;
+            }
+            utxoSet.add(lastUTXO);
+            sumOfInputVals += prevTx.value;
+            i++;
+        }
+
+        // check 3 - no utxo is claimed multiple times
+        if (utxoSet.size() != tx.getInputs().size())
+            return ThreeState.FALSE;
+
+        // check 4 - non negative output values
+        for (Transaction.Output output: tx.getOutputs()) {
+            sumOfOutputVals += output.value;
+            if (output.value < 0)
+                return ThreeState.FALSE;
+        }
+
+        // check 5 - validating input values >= sum of output values
+        if (sumOfInputVals < sumOfOutputVals)
+            return ThreeState.FALSE;;
+
+        return ThreeState.TRUE;
+    }
+
+
+    private Map<ComparableTransactionInput, ArrayList<Transaction>> getInputTransactionMap(ArrayList<Transaction> iValidTxns) {
+        final Map<ComparableTransactionInput, ArrayList<Transaction>>[] retVal = new Map[]{null};
+        iValidTxns.stream().map(tx -> {
+            Map<ComparableTransactionInput, ArrayList<Transaction>> inputTxMap = new HashMap<ComparableTransactionInput, ArrayList<Transaction>>();
+            ArrayList<Transaction> at = new ArrayList<Transaction>();
+            at.add(tx);
+            for (Transaction.Input input : tx.getInputs()) {
+                inputTxMap.put(new ComparableTransactionInput(input.prevTxHash, input.outputIndex), at);
+            }
+            return inputTxMap;
+        }).reduce((reducedMap, inputTxMap) -> {
+            Set<ComparableTransactionInput> keys = inputTxMap.keySet();
+            for (ComparableTransactionInput input: keys) {
+                if (reducedMap.containsKey(input)) {
+                    ArrayList<Transaction> currTxns = reducedMap.remove(input);
+                    currTxns.addAll(inputTxMap.get(input));
+                    reducedMap.put(input, currTxns);
+                } else {
+                    reducedMap.put(input, inputTxMap.get(input));
+                }
+            }
+            return reducedMap;
+        }).ifPresent(inputTxMap -> retVal[0] = inputTxMap);
+
+        return retVal[0];
+    }
+
+    private boolean checkIfMutuallyValid(ArrayList<Transaction> goodTxnSet, Transaction tx) {
+        double sumOfInputVals = 0.0, sumOfOutputVals = 0.0;
+        goodTxnSet.add(tx);
+        Map<ComparableTransactionInput, ArrayList<Transaction>> inputTransactionMap = getInputTransactionMap(goodTxnSet);
+
+        for (ComparableTransactionInput input : inputTransactionMap.keySet()) {
+            UTXO lastUTXO = new UTXO(input.prevTxHash, input.outputIndex);
+            Transaction.Output prevTx = pool.getTxOutput(lastUTXO);
+            sumOfInputVals += prevTx.value;
+        }
+
+        for (Transaction gTxn : goodTxnSet) {
+            for (Transaction.Output output: gTxn.getOutputs()) {
+                sumOfOutputVals += output.value;
+            }
+        }
+
+        if (sumOfInputVals < sumOfOutputVals)
+            return false;
+
+        return true;
+    }
+
     /**
      * Handles each epoch by receiving an unordered array of proposed transactions, checking each
      * transaction for correctness, returning a mutually valid array of accepted transactions, and
      * updating the current ScroogeCore.UTXO pool as appropriate.
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
-        ArrayList<Transaction> validTxns = new ArrayList<>();
-        for (Transaction tx: possibleTxs)
-            validTxns.add(tx);
+        UTXOPool tempPool = new UTXOPool(pool);
 
+        ArrayList<Transaction> mValidTxns = new ArrayList<>(); // mutually valid txns
+        ArrayList<Transaction> ignoredValidTxns = new ArrayList<>();  // independently valid txns
+        ArrayList<Transaction> iValidTxns = new ArrayList<>();  // independently valid txns
+        ArrayList<Transaction> pendingTxns = new ArrayList<>();  // txns which may depend on other txns in this set
+
+        // filter out transactions that depend on the current set of transactions.. these will be looked at last
+        // definition of depends - tx ref is of one that doesn't exist in utxo pool
+        // step 1 find out independently valid transactions and possible dependent txns..
+        for (Transaction tx: possibleTxs) {
+            if (isValidTxV2(tx) == ThreeState.TRUE)
+                iValidTxns.add(tx);
+            else if (isValidTxV2(tx) == ThreeState.MAYBE)
+                pendingTxns.add(tx);
+        }
+
+        //Map<ComparableTransactionInput, ArrayList<Transaction>> inputTransactionMap = getInputTransactionMap(iValidTxns);
+
+        // find out simple transactions which have inputs that appear once in the set
+//        txLoop:
+//        for (Transaction tx: iValidTxns) {
+//            for (Transaction.Input input: tx.getInputs()) {
+//                ComparableTransactionInput cti = new ComparableTransactionInput(input.prevTxHash, input.outputIndex);
+//                if (inputTransactionMap.get(cti).size() != 1) {
+//                    continue txLoop;
+//                }
+//            }
+//            mValidTxns.add(tx);
+//            iValidTxns.remove(tx);
+//        }
+
+        // find out transactions which have inputs which all tally
+        while (iValidTxns.size() != 0){
+            Transaction tx = iValidTxns.get(0);
+            if (checkIfMutuallyValid(mValidTxns, tx)) {
+                mValidTxns.add(tx);
+                iValidTxns.remove(0);
+            } else {
+                ignoredValidTxns.add(tx);
+                iValidTxns.remove(0);
+            }
+        }
 
         return null;
     }
+
+
+    private class ComparableTransactionInput {
+        public byte[] prevTxHash;
+        public int outputIndex;
+
+        public ComparableTransactionInput(byte[] prevHash, int index) {
+            if (prevHash == null)
+                prevTxHash = null;
+            else
+                prevTxHash = Arrays.copyOf(prevHash, prevHash.length);
+            outputIndex = index;
+        }
+
+        public boolean equals(ComparableTransactionInput other) {
+            if (other != null && Arrays.equals(other.prevTxHash, prevTxHash) && other.outputIndex == outputIndex)
+                return true;
+            else
+                return false;
+        }
+
+        public int hashCode() {
+            int sum = outputIndex;
+            for (byte b: prevTxHash) sum += b;
+
+            return sum;
+        }
+    }
+
 }
